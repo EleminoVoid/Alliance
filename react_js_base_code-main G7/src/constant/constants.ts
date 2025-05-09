a6ab8ffc607891d7dd91cbf41e08e6d7321339df
KelvinClein <58171696+Kevin530882@users.noreply.github.com>
2025-05-09 21:56:16 +0800

    added more options in sidebar

--- react_js_base_code-main G7/src/constant/constants.ts
+++ react_js_base_code-main G7/src/constant/constants.ts
@@ -42,8 +42,16 @@ export const PATHS = {
 // SIDE BAR MENU PATH
 export const SIDE_BAR_MENU = [
   {
-    path: "/dashboard",
-    label: "Dashboard"
+    path: "/profile",
+    label: "Edit Profile"
+  },
+  {
+    path: "/bookings",
+    label: "View Bookings"
+  },
+  {
+    path: "/settings",
+    label: "Settings"
   },
   {
     path: "/logout",
